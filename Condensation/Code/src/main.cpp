716960e97c59be2a3624e75ad8e34e732a346050
imamkhairi <imamkhairilubis@gmail.com>
2023-09-28 13:55:34 +0900

    add condensation

---
+++ Condensation/Code/src/main.cpp
@@ -0,0 +1,162 @@
+#include <Model.h>
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+// Plane Vertices
+GLfloat vertices[] = 
+{
+	// Coordinates      	/ Texture Coordinate
+	-1.0f, -1.0f, 0.0f, 	 0.0f,  0.0f,
+	-1.0f,  1.0f, 0.0f, 	 0.0f,  1.0f,
+	 1.0f,  1.0f, 0.0f, 	 1.0f,  1.0f,
+	 1.0f, -1.0f, 0.0f, 	 1.0f,  0.0f
+};
+
+// Plane Indices
+GLuint indices[] = {
+	0, 2, 1, // upper triangle
+	0, 3, 2  // lower triangle
+};
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what OpenGL version  3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 800 by 800 pixels
+	GLFWwindow* window = glfwCreateWindow(width, height, "Condensation", NULL, NULL);
+
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, width, height);
+
+	// Generates Shader object using shaders default.vert and default.frag
+	Shader shaderProgram("../Shaders/default.vert", "../Shaders/default.frag");
+
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	// glm::vec4 lightColor = glm::vec4(0.3f, 0.47f, 0.78f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	shaderProgram.Activate();
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS); // option ini masih banyak yg lain, refer to documentation
+
+	// Creates camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	// Load in a model
+	Model treeModel("../Models/trees/scene.gltf");
+
+	// Buffers
+	unsigned int VAO, VBO, EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
+	// Unbind
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	
+	// Texture 
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("../Textures/pop_cat.png", &widthImg, &heightImg, &numColCh, 0);
+	
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_TEXTURE_2D, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	
+	stbi_image_free(bytes);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint texUni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(texUni, 0);
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
+		// Clean the back buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+			// setting cam
+
+		// Draw a model
+		treeModel.Draw(shaderProgram, camera);
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	shaderProgram.Delete();
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
