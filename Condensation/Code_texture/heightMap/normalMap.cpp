ac21ff74ffdf674f32e2bf260a7add79dc44690f
imamkhairi <imamkhairilubis@gmail.com>
2023-10-17 19:42:18 +0900

    error normal map result

--- Condensation/Code_texture/heightMap/normalMap.cpp
+++ Condensation/Code_texture/heightMap/normalMap.cpp
@@ -14,11 +14,11 @@ const int width = 256;
 void checkCoordinate(int *x, int *y) {
     if (*x < 0) *x += 1;
     if (*y < 0) *y += 1;
-    if (*x > width) *x -= 1;
-    if (*y > height) *y -= 1;
+    if (*x > width - 1) *x -= 1;
+    if (*y > height - 1) *y -= 1;
 }
 
-void addOffset(int *value) {
+void offsetAndClamp(int *value) {
     *value += 128;
     if (*value < 0) *value = 0;
     else if(*value > 255) *value = 255;
@@ -43,33 +43,46 @@ int main() {
 
     cv::Mat heightMap = cv::imread("test.jpeg", cv::IMREAD_GRAYSCALE);
     
-    // cv::Mat normalMap = cv::Mat::zeros(3, 3, CV_8UC3);
-    // for (int y = 0; y < height; y++) {
-    //     for (int x = 0; x < width * 3; x) {
-    //         normalMap.at<unsigned char>(y,x++) = 100;
-    //         normalMap.at<unsigned char>(y,x++) = 255;
-    //         normalMap.at<unsigned char>(y,x++) = 0;
-    //     }
-    // }
+    cv::Mat normalMap = cv::Mat::zeros(width, height, CV_8UC3);
+    
 
-    cv::Mat test = cv::Mat::zeros(width, height, CV_8UC1);
+    cv::Mat grad_x = cv::Mat::zeros(width, height, CV_8UC1);
+    cv::Mat grad_y = cv::Mat::zeros(width, height, CV_8UC1);
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            int value = 0;
+            int value_x = 0;
+            int value_y = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     int x_i = x-1+i;
                     int y_i = y-1+j;
                     checkCoordinate(&x_i, &y_i);
-                    value += sobel_y[i][j] * heightMap.at<unsigned char>(y_i,x_i);
+                    value_x += sobel_x[i][j] * heightMap.at<unsigned char>(y_i,x_i);
+                    value_y += sobel_y[i][j] * heightMap.at<unsigned char>(y_i, x_i);
                 }
             }
-            addOffset(&value);
-            test.at<unsigned char>(y,x) = value;
+            offsetAndClamp(&value_x);
+            offsetAndClamp(&value_y);
+            grad_x.at<unsigned char>(y,x) = value_x;
+            grad_y.at<unsigned char>(y,x) = value_y;
         }
     }
 
-    cv::imwrite("normal.jpeg", test);
+    int i = 0;
+    int j = 0;
+    std::cout << normalMap.size().width << std::endl;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width * 3 ; x +=3) {
+            normalMap.at<unsigned char>(y,x) = 255;
+            normalMap.at<unsigned char>(y,x+1) = grad_y.at<unsigned char>(y, i++);
+            normalMap.at<unsigned char>(y,x+2) = grad_x.at<unsigned char>(y, j++);
+            // std::cout << x << std::endl;
+        }
+    }
+
+    // cv::imwrite("grad_x.jpeg", grad_x);
+    // cv::imwrite("grad_y.jpeg", grad_y);
+    cv::imwrite("normal.png", normalMap);
     return 0;
 }
