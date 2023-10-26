3a96c15301776c5daf5bb11dee42319fcdf9b53d
imamkhairi <imamkhairilubis@gmail.com>
2023-10-26 18:14:23 +0900

    add framebuffer shader

--- Condensation/Code_texture/src/main.cpp
+++ Condensation/Code_texture/src/main.cpp
@@ -5,8 +5,8 @@
 
 const unsigned int width = 1000;
 const unsigned int height = 1000;
-const unsigned int texWidth = 500;
-const unsigned int texHeight = 500;
+const unsigned int texWidth = 1000;
+const unsigned int texHeight = 1000;
 
 //  =========================  Not Used (old plane vertices)
 // // Plane Vertices
