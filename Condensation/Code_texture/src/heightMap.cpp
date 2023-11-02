11e9bbaaa0fc2af36f4a8dc44274c1f7b9181ccc
カイリ <p214071@kushiro.kosen-ac.jp>
2023-11-02 13:58:18 +0900

    worked updateHeightMap()

--- Condensation/Code_texture/src/heightMap.cpp
+++ Condensation/Code_texture/src/heightMap.cpp
@@ -1,33 +1,43 @@
 #include <heightMap.h>
 
-heightMap::heightMap(std::vector <Droplet> ParticleSystem, int mapHeight, int mapWidth) {
+heightMap::heightMap(particleSystem *ParticleSystem, int mapHeight, int mapWidth) {
     this->mapHeight = mapHeight;
     this->mapWidth = mapWidth;
     this->generateHeightMap(ParticleSystem);
 }
 
-void heightMap::generateHeightMap(std::vector <Droplet> particleSystem) {
-    cv::Mat heightMap = cv::Mat::zeros(this->mapWidth, this->mapHeight, CV_8UC1);
-
-    for (auto & particle : particleSystem) {
+void heightMap::drawHeightMap(cv::Mat dst, std::vector <Droplet> PS, int start, int end) {
+    for (int i = start; i < end; i++) {
         // starting point
-        int x = particle.position.x - (int)particle.radius - 2;
-        int y = particle.position.y - (int)particle.radius - 2;
+        int x = PS[i].position.x - (int)PS[i].radius - 2;
+        int y = PS[i].position.y - (int)PS[i].radius - 2;
         // end point
-        int x1 = particle.position.x + (int)particle.radius + 2;
-        int y1 = particle.position.y + (int)particle.radius + 2;
+        int x1 = PS[i].position.x + (int)PS[i].radius + 2;
+        int y1 = PS[i].position.y + (int)PS[i].radius + 2;
 
         for (int y0 = y; y0 <= y1; y0++) {
             for (int x0 = x; x0 <= x1; x0++) {
-                float h = calcHeight(particle, x0, y0);
+                float h = calcHeight(PS[i], x0, y0);
                 if ( h > 0 ) {
-                    heightMap.at<unsigned char>(y0, x0) = h * 10; // 50 konstansta
+                    dst.at<unsigned char>(y0, x0) = h * 10; // 50 konstansta
                 }
             }
         }
     }
+}
 
-    this->smoothingHeightMap(heightMap);
+void heightMap::generateHeightMap(particleSystem *PS) {
+    cv::Mat heightMap = cv::Mat::zeros(this->mapWidth, this->mapHeight, CV_8UC1);
+
+    std::vector <Droplet> particle =  PS->getParticleSystem(); // bisa pake auto
+    int start = PS->getDrewAmmount();
+    int end = PS->getParticleAmmount();
+    this->drawHeightMap(heightMap, particle, start, end);
+
+    PS->setDrewAmmount(PS->getParticleAmmount());
+
+    // this->smoothingHeightMap(heightMap);
+    cv::imwrite("../Textures/heightMap.png", heightMap);
 }
 
 float heightMap::calcHeight(Droplet a, int x_i, int y_i) {
@@ -40,9 +50,9 @@ float heightMap::calcHeight(Droplet a, int x_i, int y_i) {
     } else return 0;
 }
 
+// PC lab smoothing = 110ms
 void heightMap::smoothingHeightMap(cv::Mat heightMap) {
     cv::Mat smoothed  = cv::Mat::zeros(this->mapHeight, this->mapHeight, CV_8UC1);
-
     for (int y = 0; y < this->mapHeight; y++) {
         for (int x = 0; x < this->mapWidth; x++) {
             float value = 0;
@@ -55,13 +65,29 @@ void heightMap::smoothingHeightMap(cv::Mat heightMap) {
                 }
             }
             value = value/9;
-            // if (value < 1 && value > 0)  std::cout << value << std::endl;
             heightThreshold(&value);
             smoothed.at<unsigned char>(y,x) = value;
         }
     }
 
     cv::imwrite("../Textures/heightMap.png", smoothed);
+}
+
+void heightMap::updateHeightMap(particleSystem *PS)
+{
+    int last = PS->getDrewAmmount();
+    int ammount = PS->getParticleAmmount();
+    cv::Mat heightMap = cv::imread("../Textures/heightMap.png", cv::IMREAD_GRAYSCALE);
+
+    if (last < ammount) {
+        int dif = ammount - last;
+        std::vector <Droplet> particle =  PS->getParticleSystem(); // bisa pake auto
+        this->drawHeightMap(heightMap, particle, last, ammount);
+    }
+    
+    PS->setDrewAmmount(PS->getParticleAmmount());
+
+    cv::imwrite("../Textures/heightMap.png", heightMap);
 }
 
 void heightMap::checkCoordinate(int *x, int *y) {
--- Condensation/Code_texture/src/main.cpp
+++ Condensation/Code_texture/src/main.cpp
@@ -119,11 +119,15 @@ int main()
 		return -1;
 	}
 	
-	particleSystem ParticleSystem(150, texHeight, texWidth);
+	particleSystem ParticleSystem(2, texHeight, texWidth);
 
-    clock_t tStart = clock();
-	heightMap HeightMap(ParticleSystem.getParticleSystem(), texHeight, texWidth);
-    printf("Height Map: %.5f ms\n", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000));
+    // clock_t tStart = clock();
+	heightMap HeightMap(&ParticleSystem, texHeight, texWidth);
+	ParticleSystem.addParticle(3);
+	HeightMap.updateHeightMap(&ParticleSystem);
+    // printf("Height Map: %.5f ms\n", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000));
+	// std::cout << ParticleSystem.getParticleAmmount() << std::endl;
+    // std::cout << ParticleSystem.getDrewAmmount() << std::endl;
 
 	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
