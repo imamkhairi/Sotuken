1876f8e7d741759fb43c11fb28213097a7451b74
imamkhairi <imamkhairilubis@gmail.com>
2023-11-05 19:25:49 +0900

    fft not working

--- Condensation/Code_texture(test)/heightMap/main.cpp
+++ Condensation/Code_texture(test)/heightMap/main.cpp
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 
-const unsigned int texWidth  = 200;
-const unsigned int texHeight = 200;
+const unsigned int texWidth  = 1024;
+const unsigned int texHeight = 1024;
 
 int main() {
     clock_t tStart = clock();
@@ -12,16 +12,20 @@ int main() {
     // printf("Particle System: %.5f ms\n", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000));
 
 	heightMap HeightMap(&ParticleSystem, texHeight, texWidth);
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 100; i++) {
         tStart = clock();
         ParticleSystem.addParticle(1);
         HeightMap.updateHeightMap(&ParticleSystem);
         printf("Height Map: %.5f ms\n", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000));
         usleep(50000); 
     }
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 20; i++) {
+        tStart = clock();
         HeightMap.smoothingHeightMap("heightMap.png", &ParticleSystem);
+        printf("Smoothing: %.5f ms\n", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000));
+
     }
+
     std::cout << ParticleSystem.getParticleAmmount() << std::endl;
     std::cout << ParticleSystem.getDrewAmmount() << std::endl;
 }
