2a5766662b7e046c4e747cdc2970582992398aa5
カイリ <p214071@kushiro.kosen-ac.jp>
2023-12-12 13:09:05 +0900

    update fiel

---
+++ Condensation/Code_texture/include/heightMap.h
@@ -0,0 +1,37 @@
+#ifndef heightMap_CLASS_H
+#define heightMap_CLASS_H
+
+#include <opencv2/opencv.hpp>
+#include <particleSystem.h>
+#include <IDMap.h>
+
+
+const int avg[3][3] = {
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1
+};
+
+
+class heightMap {
+    public:
+        heightMap(particleSystem *ParticleSystem, int mapHeight, int mapWidth);
+    
+        void smoothingHeightMap(IDMap idMap, particleSystem *PS);
+        void updateHeightMap(particleSystem *PS);
+
+    private:
+        int mapHeight;
+        int mapWidth;
+
+        float calcHeight(Droplet a, int x_i, int y_i);
+        void generateHeightMap(particleSystem *ParticleSystem);
+        // void smoothingHeightMap(cv::Mat heightMap);
+        void checkCoordinate(int *x, int *y);
+        int heightThreshold(float *value);
+        void drawHeightMap(cv::Mat dst, std::vector <Droplet> particle, int start, int end);
+        float distance(float x1, float y1, float x2, float y2);
+
+};
+
+#endif
---
+++ Condensation/Code_texture/src/particleSystem.cpp
@@ -0,0 +1,64 @@
+#include <particleSystem.h>
+
+particleSystem::particleSystem(int particleAmmount, int mapHeight, int mapWidth) {
+	srand(time(0));
+    
+    this->particleAmmount = particleAmmount;
+    this->mapHeigth = mapHeight;
+    this->mapWidth = mapWidth;
+    this->initiateParticleSystem(&this->Particles); 
+    this->drewAmmount = 0;
+};
+
+void particleSystem::initiateParticleSystem(std::vector <Droplet> *Particles) {
+    for (int i = 0; i < this->particleAmmount; i++) {
+        Droplet a;
+        this->initiateDroplet(&a);
+        Particles->push_back(a);
+    }
+}
+
+void particleSystem::initiateDroplet(Droplet *a) {    
+    a->mass = 500.0f;
+    calcRadius(a);
+    a->position =  glm::vec3((rand() % (int)(this->mapWidth - 2*(a->radius + 2))) + (int)(a->radius + 2), 
+        (rand() % (int)(this->mapHeigth - 2*(a->radius + 2))) + (int)(a->radius + 2), 
+        10);
+}
+
+void particleSystem::calcRadius(Droplet *a) {
+    a->radius = std::cbrt((double)(3*a->mass)/(double)(2*a->density*M_1_PI));
+}
+
+
+void particleSystem::addParticle(int ammount)
+{   
+    if(this->particleAmmount < 400) {
+        // std::cout << this->particleAmmount << std::endl;
+        this->particleAmmount += ammount;
+        for (int i = 0; i < ammount; i++) {
+            Droplet a;
+            this->initiateDroplet(&a);
+            this->Particles.push_back(a);
+        }
+    }
+}
+
+void particleSystem::setDrewAmmount(int value)
+{
+    this->drewAmmount = value;
+}
+
+std::vector <Droplet> particleSystem::getParticleSystem() {
+    return this->Particles;
+}
+
+int particleSystem::getParticleAmmount()
+{
+    return this->particleAmmount;
+}
+
+int particleSystem::getDrewAmmount() 
+{
+    return this->drewAmmount;
+}
